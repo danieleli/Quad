22aa2d6bdcb7e2ade515f88ef031525d350acbc0
Dan Schlossberg <danschlossberg@gmail.com>
2012-10-10 21:06:28 -0400

    add twitter backbone poc

---
+++ test/poc/twitter_spec.js
@@ -0,0 +1,54 @@
+var $ = require('jquery'),
+  Backbone = require('backbone'),
+  vows = require('vows'),
+  assert = require('assert'),
+  twitter = require('../../lib/poc/twitter');
+
+Backbone.setDomLibrary($);
+var tweets = new twitter.Tweets();
+
+var jqXHR;
+var suite = vows.describe('twitter models')
+
+  .addBatch({
+    'Tweet model' : {
+      topic                       : function () {
+        return new twitter.Tweet({name : "some bs"});
+      },
+      'is defined'                : function (topic) {
+        assert.isDefined(twitter);
+        assert.notEqual(twitter, undefined);
+        assert.notEqual(twitter.Tweet, undefined);
+        assert.notEqual(topic, null);
+      },
+      'is a function'             : function (topic) {
+        assert.isFunction(twitter.Tweet);
+      },
+      'is returns default values' : function (topic) {
+        assert.equal(topic.get('content'), "default");
+      },
+
+      'is returns default newly initialized values' : function (topic) {
+        assert.equal(topic.get('name'), "some bs");
+      }
+    }})
+
+  .addBatch({
+    'Tweets collection' : {
+      topic : function () {
+        jqXHR = tweets.fetch({success : this.callback});
+        //console.log('jqXHR:' + jqXHR);
+      },
+
+      'can fetch tweets' : function (collection, response, x) {
+        console.log('response: ' + response);
+        console.log('collection: ' + collection);
+        console.log('tweets: ' + tweets.length);
+
+        tweets.each(function (item, index, array) {
+          console.log("tweet: " + JSON.stringify(item));
+        });
+      }
+    }})
+
+  .run();

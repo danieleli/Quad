c6c1caa076d71b68f07c2eb139f4abb530db32bb
Dan Schlossberg <danschlossberg@gmail.com>
2012-10-12 16:10:06 -0400

    update oauth test

--- test/poc/backbone_oauth_google/contactModels.js
+++ test/poc/backbone_oauth_google/contactModels.js
@@ -15,5 +15,51 @@ var Contacts = Backbone.Collection.extend( {
         parse: function ( resp, xhr ) {
             return resp.results;
         }
+        //        ,
+        //        sync : function ( method, model, options ) {
+        //            var type = methodMap[method];
+        //
+        //            // Default options, unless specified.
+        //            options || (options = {});
+        //
+        //            // Default JSON-request options.
+        //            var params = {type: type, dataType: 'json'};
+        //
+        //            // Ensure that we have a URL.
+        //            if ( !options.url ) {
+        //                params.url = getValue( model, 'url' ) || urlError();
+        //            }
+        //
+        //            // Ensure that we have the appropriate request data.
+        //            if ( !options.data && model && (method == 'create' || method == 'update') ) {
+        //                params.contentType = 'application/json';
+        //                params.data = JSON.stringify( model.toJSON() );
+        //            }
+        //
+        //            // For older servers, emulate JSON by encoding the request into an HTML-form.
+        //            if ( Backbone.emulateJSON ) {
+        //                params.contentType = 'application/x-www-form-urlencoded';
+        //                params.data = params.data ? {model: params.data} : {};
+        //            }
+        //
+        //            // For older servers, emulate HTTP by mimicking the HTTP method with `_method`
+        //            // And an `X-HTTP-Method-Override` header.
+        //            if ( Backbone.emulateHTTP ) {
+        //                if ( type === 'PUT' || type === 'DELETE' ) {
+        //                    if ( Backbone.emulateJSON ) params.data._method = type;
+        //                    params.type = 'POST';
+        //                    params.beforeSend = function ( xhr ) {
+        //                        xhr.setRequestHeader( 'X-HTTP-Method-Override', type );
+        //                    };
+        //                }
+        //            }
+        //            if ( params.type !== 'GET' && !Backbone.emulateJSON ) {
+        //                params.processData = false;
+        //            }
+        //            return $.ajax( _.extend( params, options ) );
+        //
     }
 );
+
+exports.Contact = Contact;
+exports.Contacts = Contacts;
--- test/poc/backbone_oauth_google/test.js
+++ test/poc/backbone_oauth_google/test.js
@@ -2,50 +2,118 @@ var OAuth = require( 'oauth' ).OAuth,
     $ = require( 'jquery' ),
     Backbone = require( 'backbone' ),
     Contacts = require( './contactModels' ),
-    mockState = require( './mockSessionState' );
+    mockState = require( './mockSessionState' ),
+    assert = require( 'assert' ),
+    vows = require( 'vows' );
 
 
 Backbone.setDomLibrary( $ );
 
 $.ajaxPrefilter( function ( options, originalOptions, jqXHR ) {
         console.log( '---------------prefilter------------------' );
+        console.log( "options: " + JSON.stringify( options ) );
+        console.log( '---------------' );
+        console.log( "originalOptions: " + JSON.stringify( originalOptions ) );
+        console.log( '---------------' );
+        console.log( "jqXHR : " + JSON.stringify( jqXHR ) );
+        jqXHR.setRequestHeader( "TEST", ".net rocks!" );
     }
 );
+//
+//exports.testGoogleCall = function ( test ) {
+//    var oa = new OAuth(
+//        "", // mockState.OA._requestUrl,
+//        "", // mockState.OA._accessUrl,
+//        mockState.OA._consumerKey,
+//        mockState.OA._consumerSecret,
+//        "", //mockState.OA._version,
+//        "", //mockState.OA._authorize_callback,
+//        mockState.OA._signatureMethod
+//    );
+//
+//    // Example using GData API v3
+//    // GData Specific Header
+//    oa._headers['GData-Version'] = '3.0';
+//
+//    oa.getProtectedResource(
+//        "https://www.google.com/m8/feeds/contacts/default/full?alt=json",
+//        "GET",
+//        mockState.OAuth.oauth_access_token,
+//        mockState.OAuth.oauth_access_token_secret,
+//        function ( error, data, response ) {
+//            var feed = JSON.parse( data );
+//            var entries = feed.feed.entry;
+//
+//            // loop through and log entries
+//            for ( var x = 0; x <= entries.length; x++ ) {
+//                console.log( entries[x] );
+//                console.log( '---------------------------------------------------' );
+//            }
+//        }
+//    );
+//
+//
+//    test.expect( 1 );
+//    test.ok( true, "this assertion should pass" );
+//    test.done();
+//};
+//
 
-exports.testGoogleCall = function ( test ) {
-    var oa = new OAuth(
-        "", // mockState.OA._requestUrl,
-        "", // mockState.OA._accessUrl,
-        mockState.OA._consumerKey,
-        mockState.OA._consumerSecret,
-        "", //mockState.OA._version,
-        "", //mockState.OA._authorize_callback,
-        mockState.OA._signatureMethod
-    );
+// function wrapper for node unit
+//exports.testContactsCollection = function ( te  t ) {
+var oa = new OAuth(
+    "", // mockState.OA._requestUrl,
+    "", // mockState.OA._accessUrl,
+    mockState.OA._consumerKey,
+    mockState.OA._consumerSecret,
+    "", //mockState.OA._version,
+    "", //mockState.OA._authorize_callback,
+    mockState.OA._signatureMethod
+);
+oa._headers['GData-Version'] = '3.0';
 
-    // Example using GData API v3
-    // GData Specific Header
-    oa._headers['GData-Version'] = '3.0';
+//    oa.getProtectedResource(
+//        "https://www.google.com/m8/feeds/contacts/default/full?alt=json",
+//        "GET",
+//        mockState.OAuth.oauth_access_token,
+//        mockState.OAuth.oauth_access_token_secret,
+//        function ( error, data, response ) {
+//            var feed = JSON.parse( data );
+//            var entries = feed.feed.entry;
+//
+//            // loop through and log entries
+//            for ( var x = 0; x <= entries.length; x++ ) {
+//                console.log( entries[x] );
+//                console.log( '---------------------------------------------------' );
+//            }
+//        }
+//    );
 
-    oa.getProtectedResource(
-        "https://www.google.com/m8/feeds/contacts/default/full?alt=json",
-        "GET",
-        mockState.OAuth.oauth_access_token,
-        mockState.OAuth.oauth_access_token_secret,
-        function ( error, data, response ) {
-            var feed = JSON.parse( data );
-            var entries = feed.feed.entry;
+var contacts = new Contacts.Contacts();
 
-            // loop through and log entries
-            for ( var x = 0; x <= entries.length; x++ ) {
-                console.log( entries[x] );
-                console.log( '---------------------------------------------------' );
-            }
+
+vows.describe( 'Contacts' ).addBatch( {
+    'Fetch': {
+        topic            : function () {
+            contacts.fetch(
+                {
+                    success: this.callback,
+                    error  : this.callback
+                }
+            );
+        },
+        'populates self ': function ( collection, response, x ) {
+
+            console.log( JSON.stringify( collection ) + " " );
+            console.log( "response=" + response );
+            console.log( "vows atest" );
+            assert.equal( response.status, 200 );
         }
-    );
+    }
+} ).run();
 
+test.expect( 1 );
+test.ok( true, "this assertion should pass" );
+test.done();
 
-    test.expect( 1 );
-    test.ok( true, "this assertion should pass" );
-    test.done();
-};
+//};

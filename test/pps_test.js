635befe6b31f1eed029573372126260b527011e2
Dan Schlossberg <danschlossberg@gmail.com>
2012-10-29 13:28:44 -0400

    convert tests to vows

--- test/pps_test.js
+++ test/pps_test.js
@@ -1,41 +1,39 @@
-var assert = require( 'chai' ).assert,
+var vows = require( 'vows' ),
+    assert = require( 'chai' ).assert,
     Pps = require( '../lib/pps' ),
     apiConfig = require( './helpers/api_config' );
 
 var pps = new Pps( apiConfig );
 
-
-describe( 'pps object', function () {
-    describe( 'has pci property', function () {
-        it( 'that returns pcis when .getAll() is called', function ( done ) {
-            pps.Pci.getAll( null, function ( error, data, response ) {
-                assert.isNull( error, JSON.stringify( error ) );
-                assert.ok( data );
-                console.log( JSON.stringify( data ) );
-                done();
-            } );
-        } );
-    } );
-
-    describe( 'has NotificationCategory property', function () {
-        it( 'that returns NotificationCategories when .getAll() is called', function ( done ) {
-            pps.NotificationCategory.getAll( null, function ( error, data, response ) {
-                assert.isNull( error, JSON.stringify( error ) );
-                assert.ok( data );
-                console.log( JSON.stringify( data ) );
-                done();
-            } );
-        } );
-    } );
-
-    describe( 'has Account property', function () {
-        it( 'that returns Accounts when .getAll() is called', function ( done ) {
-            pps.Account.getAll( null, function ( error, data, response ) {
-                assert.isNull( error, JSON.stringify( error ) );
-                assert.ok( data );
-                console.log( JSON.stringify( data ) );
-                done();
-            } );
-        } );
-    } );
-} );
+vows.describe( 'PPS data accessor' )
+    .addBatch( { 'has a PCI property': {
+        topic                                       : function () {
+            pps.Pci.getAll( null, this.callback )
+        },
+        'that returns pcis when .getAll() is called': function ( error, data, response ) {
+            assert.isNull( error, JSON.stringify( error ) );
+            assert.ok( data );
+        //    console.log( JSON.stringify( data ) );
+        }
+    }} )
+    .addBatch( { 'has a NotificationCategory property': {
+        topic                                       : function () {
+            pps.NotificationCategory.getAll( null, this.callback )
+        },
+        'that returns NotificationCategories when .getAll() is called': function ( error, data, response ) {
+            assert.isNull( error, JSON.stringify( error ) );
+            assert.ok( data );
+         //   console.log( JSON.stringify( data ) );
+        }
+    }} )
+    .addBatch( { 'has a Account property': {
+        topic                                       : function () {
+            pps.Account.getAll( null, this.callback )
+        },
+        'that returns Accounts when .getAll() is called': function ( error, data, response ) {
+            assert.isNull( error, JSON.stringify( error ) );
+            assert.ok( data );
+         //   console.log( JSON.stringify( data ) );
+        }
+    }} )
+    .run();
